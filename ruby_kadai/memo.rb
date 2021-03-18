88284a6fb255a34bbbc7daa9bfb104ac1f92f519
EC2 Default User <ec2-user@ip-172-31-30-68.ec2.internal>
2021-03-18 14:32:13 +0000

    revised-commit

--- ruby_kadai/memo.rb
+++ ruby_kadai/memo.rb
@@ -4,24 +4,38 @@ puts "1(新規でメモを作成) 2(既存のメモを編集する)"
 
 memo_type = gets.to_i
 
-if memo_type = "1"
+if memo_type == 1
   puts "拡張子を除いたファイル名を入力してください"
-  file_name = gets.chomp
-  
-  file = File.open("#{file_name}.csv", "a")
-  
+  file_name = gets.chomp + ".csv"
   puts "メモしたい内容を入力してください"
   puts "完了したらCtrl + Dを押します"
-  memo_message = gets
-  file.puts "#{memo_message}"
+  array = []
   
-  elsif memo_type == "2"
-    puts "編集ファイルを入力してください"
-    file_name = gets
-    file = File.open("#{file_name}.csv", "a")
-    puts "メモしたい内容を入力してください"
-    puts "完了したらCtrl + Dを押します"
-    memo_message = gets
-    file.puts "#{memo_message}"
-    
+  while sentence = gets
+    array << sentence.chomp
   end
+  
+  CSV.open(file_name, "w") do |csv|
+    csv << array
+  end
+  
+  elsif memo_type == 2
+    puts "既存のメモの拡張子を除いたファイル名を入力してください"
+    file_name = gets.chomp + ".csv"
+    puts "追記したい内容を記入してください"
+    puts "完了したらCtrl + Dを押します"
+    
+    array = []
+    
+    while sentence = gets
+      array << sentence.chomp
+    end
+    
+    CSV.open(file_name, "a") do |csv|
+      csv << array
+    end
+    
+  else 
+    puts "1か2いずれかの数値を入力してください"
+  end
+    
